f87fc7ec28e83491763771e4c56996aaa74af7c1
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-03-10 19:06:11 +0100

    implement automatic start and kill of geckodriver

--- shell.nix
+++ shell.nix
@@ -7,4 +7,11 @@
     openssl
     pkg-config
   ];
+  shellHook = ''
+    # In your shellHook:
+    geckodriver > geckodriver.log 2>&1 &
+    GECKODRIVER_PID=$!
+    trap "kill $GECKODRIVER_PID" EXIT
+    nu
+  '';
 }
